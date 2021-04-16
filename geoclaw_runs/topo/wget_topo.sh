bb163231ff989bc9559ec27642be1753923e9937
Randy LeVeque <rjl@uw.edu>
2021-04-16 07:33:32 -0700

    update wget_topo.sh for DiscoBay

--- geoclaw_runs/topo/wget_topo.sh
+++ geoclaw_runs/topo/wget_topo.sh
@@ -7,4 +7,5 @@ wget http://depts.washington.edu/ptha/topo/etopo1_-163_-122_38_63.asc
 wget http://depts.washington.edu/ptha/topo/SJdF_2sec_center.asc
 wget http://depts.washington.edu/ptha/topo/PT_2sec_center.asc
 wget http://depts.washington.edu/ptha/topo/PS_2sec_center.asc
+wget http://depts.washington.edu/ptha/topo/DiscoveryBay_1_3s_center.asc
 
